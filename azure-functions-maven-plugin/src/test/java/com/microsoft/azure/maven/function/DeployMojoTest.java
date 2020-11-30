1f1a7d63c45f966a83c0ebd56f9515e798e08d3a
a888882 <70435483+a888882@users.noreply.github.com>
2020-11-30 18:50:24 +0800

    Initial commit

---
+++ azure-functions-maven-plugin/src/test/java/com/microsoft/azure/maven/function/DeployMojoTest.java
@@ -0,0 +1,268 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.maven.function;
+
+import com.microsoft.azure.common.appservice.DeployTargetType;
+import com.microsoft.azure.common.appservice.DeploymentSlotSetting;
+import com.microsoft.azure.common.appservice.DeploymentType;
+import com.microsoft.azure.common.deploytarget.DeployTarget;
+import com.microsoft.azure.common.exceptions.AzureExecutionException;
+import com.microsoft.azure.common.function.handlers.artifact.MSDeployArtifactHandlerImpl;
+import com.microsoft.azure.common.function.handlers.runtime.FunctionRuntimeHandler;
+import com.microsoft.azure.common.function.handlers.runtime.WindowsFunctionRuntimeHandler;
+import com.microsoft.azure.common.function.utils.FunctionUtils;
+import com.microsoft.azure.common.handlers.ArtifactHandler;
+import com.microsoft.azure.common.handlers.artifact.FTPArtifactHandlerImpl;
+import com.microsoft.azure.common.handlers.artifact.ZIPArtifactHandlerImpl;
+import com.microsoft.azure.management.appservice.FunctionApp;
+import com.microsoft.azure.management.appservice.FunctionApp.DefinitionStages.WithCreate;
+import com.microsoft.azure.management.appservice.FunctionApp.Update;
+import com.microsoft.azure.management.appservice.FunctionDeploymentSlot;
+import com.microsoft.azure.maven.auth.AzureAuthFailureException;
+import com.microsoft.azure.maven.telemetry.TelemetryProxy;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+import static com.microsoft.azure.common.appservice.DeploymentType.DOCKER;
+import static com.microsoft.azure.common.appservice.DeploymentType.RUN_FROM_BLOB;
+import static com.microsoft.azure.common.appservice.DeploymentType.RUN_FROM_ZIP;
+import static com.microsoft.azure.common.appservice.OperatingSystemEnum.Docker;
+import static com.microsoft.azure.common.appservice.OperatingSystemEnum.Linux;
+import static com.microsoft.azure.common.appservice.OperatingSystemEnum.Windows;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({FunctionUtils.class})
+public class DeployMojoTest extends MojoTestBase {
+    private DeployMojo mojo = null;
+    private DeployMojo mojoSpy = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mojo = getMojoFromPom();
+        mojoSpy = spy(mojo);
+    }
+
+    @Test
+    public void getConfiguration() throws Exception {
+        assertEquals("resourceGroupName", mojo.getResourceGroup());
+
+        assertEquals("appName", mojo.getAppName());
+
+        assertEquals("westeurope", mojo.getRegion());
+    }
+
+    @Test
+    public void doExecute() throws Exception {
+        final ArtifactHandler handler = mock(ArtifactHandler.class);
+        final FunctionRuntimeHandler runtimeHandler = mock(FunctionRuntimeHandler.class);
+        final FunctionApp app = mock(FunctionApp.class);
+        doReturn(app).when(mojoSpy).getFunctionApp();
+        doReturn(app).when(mojoSpy).updateFunctionApp(app, runtimeHandler);
+        doReturn(handler).when(mojoSpy).getArtifactHandler();
+        doReturn(runtimeHandler).when(mojoSpy).getFunctionRuntimeHandler();
+        doCallRealMethod().when(mojoSpy).createOrUpdateResource();
+        final DeployTarget deployTarget = new DeployTarget(app, DeployTargetType.FUNCTION);
+        doNothing().when(mojoSpy).listHTTPTriggerUrls();
+        doNothing().when(mojoSpy).checkArtifactCompileVersion();
+        doNothing().when(mojoSpy).parseConfiguration();
+        doReturn(null).when(mojoSpy).getResourcePortalUrl(any());
+        mojoSpy.doExecute();
+        verify(mojoSpy, times(1)).createOrUpdateResource();
+        verify(mojoSpy, times(1)).doExecute();
+        verify(mojoSpy, times(1)).updateFunctionApp(any(FunctionApp.class), any());
+        verify(handler, times(1)).publish(refEq(deployTarget));
+        verifyNoMoreInteractions(handler);
+    }
+
+    @Test
+    public void createFunctionAppIfNotExist() throws Exception {
+        doReturn(null).when(mojoSpy).getFunctionApp();
+        doReturn(null).when(mojoSpy).getFunctionRuntimeHandler();
+        doReturn(null).when(mojoSpy).createFunctionApp(any());
+
+        mojoSpy.createOrUpdateResource();
+
+        verify(mojoSpy).createFunctionApp(any());
+    }
+
+    @Test
+    public void updateFunctionApp() throws Exception {
+        final FunctionApp app = mock(FunctionApp.class);
+        final Update update = mock(Update.class);
+        doNothing().when(mojoSpy).configureAppSettings(any(Consumer.class), anyMap());
+        final FunctionRuntimeHandler functionRuntimeHandler = mock(WindowsFunctionRuntimeHandler.class);
+        doReturn(update).when(functionRuntimeHandler).updateAppRuntime(app);
+        mojoSpy.updateFunctionApp(app, functionRuntimeHandler);
+
+        verify(update, times(1)).apply();
+    }
+
+    @Test
+    public void configureAppSettings() throws Exception {
+        final WithCreate withCreate = mock(WithCreate.class);
+
+        mojo.configureAppSettings(withCreate::withAppSettings, mojo.getAppSettingsWithDefaultValue());
+
+        verify(withCreate, times(1)).withAppSettings(anyMap());
+    }
+
+    @Test
+    public void testDefaultAppSettings() throws Exception {
+        final Map settings = mojo.getAppSettingsWithDefaultValue();
+        assertEquals("java", settings.get("FUNCTIONS_WORKER_RUNTIME"));
+        assertEquals("~3", settings.get("FUNCTIONS_EXTENSION_VERSION"));
+    }
+
+    @Test
+    public void testCustomAppSettings() throws Exception {
+        final DeployMojo mojoWithSettings = (DeployMojo) getMojoFromPom("/pom-with-settings.xml", "deploy");
+        final Map settings = mojoWithSettings.getAppSettingsWithDefaultValue();
+        assertEquals("bar", settings.get("FOO"));
+        assertEquals("java", settings.get("FUNCTIONS_WORKER_RUNTIME"));
+        assertEquals("beta", settings.get("FUNCTIONS_EXTENSION_VERSION"));
+    }
+
+    @Test
+    public void getMSDeployArtifactHandler() throws AzureExecutionException {
+        final TelemetryProxy mockProxy = mock(TelemetryProxy.class);
+        doReturn(mockProxy).when(mojoSpy).getTelemetryProxy();
+        doNothing().when(mockProxy).addDefaultProperty(any(), any());
+        doReturn("azure-functions-maven-plugin").when(mojoSpy).getPluginName();
+        doReturn("test-path").when(mojoSpy).getBuildDirectoryAbsolutePath();
+        doReturn(DeploymentType.MSDEPLOY).when(mojoSpy).getDeploymentType();
+        final ArtifactHandler handler = mojoSpy.getArtifactHandler();
+
+        assertNotNull(handler);
+        assertTrue(handler instanceof MSDeployArtifactHandlerImpl);
+    }
+
+    @Test
+    public void getFTPArtifactHandler() throws AzureExecutionException {
+        final TelemetryProxy mockProxy = mock(TelemetryProxy.class);
+        doReturn(mockProxy).when(mojoSpy).getTelemetryProxy();
+        doNothing().when(mockProxy).addDefaultProperty(any(), any());
+        doReturn("azure-functions-maven-plugin").when(mojoSpy).getPluginName();
+        doReturn("test-path").when(mojoSpy).getBuildDirectoryAbsolutePath();
+        doReturn(DeploymentType.FTP).when(mojoSpy).getDeploymentType();
+        final ArtifactHandler handler = mojoSpy.getArtifactHandler();
+
+        assertNotNull(handler);
+        assertTrue(handler instanceof FTPArtifactHandlerImpl);
+    }
+
+    @Test
+    public void getZIPArtifactHandler() throws AzureExecutionException {
+        final TelemetryProxy mockProxy = mock(TelemetryProxy.class);
+        doReturn(mockProxy).when(mojoSpy).getTelemetryProxy();
+        doNothing().when(mockProxy).addDefaultProperty(any(), any());
+        doReturn("azure-functions-maven-plugin").when(mojoSpy).getPluginName();
+        doReturn("test-path").when(mojoSpy).getBuildDirectoryAbsolutePath();
+        doReturn(DeploymentType.ZIP).when(mojoSpy).getDeploymentType();
+        final ArtifactHandler handler = mojoSpy.getArtifactHandler();
+
+        assertNotNull(handler);
+        assertTrue(handler instanceof ZIPArtifactHandlerImpl);
+    }
+
+    @Test(expected = AzureExecutionException.class)
+    public void getArtifactHandlerThrowException() throws Exception {
+        getMojoFromPom().getArtifactHandler();
+    }
+
+    @Test
+    public void testGetDeploymentTypeByRuntime() throws AzureExecutionException {
+        // Windows
+        doReturn(Windows).when(mojoSpy).getOsEnum();
+        assertEquals(RUN_FROM_ZIP, mojoSpy.getDeploymentTypeByRuntime());
+        // Linux
+        doReturn(Linux).when(mojoSpy).getOsEnum();
+        doReturn(true).when(mojoSpy).isDedicatedPricingTier();
+        assertEquals(RUN_FROM_ZIP, mojoSpy.getDeploymentTypeByRuntime());
+        doReturn(false).when(mojoSpy).isDedicatedPricingTier();
+        assertEquals(RUN_FROM_BLOB, mojoSpy.getDeploymentTypeByRuntime());
+        // Docker
+        doReturn(Docker).when(mojoSpy).getOsEnum();
+        assertEquals(DOCKER, mojoSpy.getDeploymentTypeByRuntime());
+    }
+
+    @Test(expected = AzureExecutionException.class)
+    public void testDeploymentSlotThrowExceptionIfFunctionNotExists() throws AzureAuthFailureException, AzureExecutionException {
+        final DeploymentSlotSetting slotSetting = new DeploymentSlotSetting();
+        slotSetting.setName("Exception");
+        doReturn(slotSetting).when(mojoSpy).getDeploymentSlotSetting();
+        doReturn(null).when(mojoSpy).getFunctionApp();
+        doReturn(null).when(mojoSpy).getFunctionRuntimeHandler();
+        doNothing().when(mojoSpy).parseConfiguration();
+        doNothing().when(mojoSpy).checkArtifactCompileVersion();
+        mojoSpy.doExecute();
+    }
+
+    @Test
+    public void testCreateDeploymentSlot() throws AzureAuthFailureException, AzureExecutionException {
+        final FunctionDeploymentSlot slot = mock(FunctionDeploymentSlot.class);
+        final DeploymentSlotSetting slotSetting = new DeploymentSlotSetting();
+        slotSetting.setName("Test");
+        doReturn(slotSetting).when(mojoSpy).getDeploymentSlotSetting();
+
+        final FunctionRuntimeHandler runtimeHandler = mock(FunctionRuntimeHandler.class);
+        final FunctionDeploymentSlot.DefinitionStages.WithCreate mockWithCreate = mock(FunctionDeploymentSlot.DefinitionStages.WithCreate.class);
+        doReturn(slot).when(mockWithCreate).create();
+        doReturn(mockWithCreate).when(runtimeHandler).createDeploymentSlot(any(), any());
+        doReturn(runtimeHandler).when(mojoSpy).getFunctionRuntimeHandler();
+
+        final ArtifactHandler artifactHandler = mock(ArtifactHandler.class);
+        doNothing().when(artifactHandler).publish(any());
+        doReturn(artifactHandler).when(mojoSpy).getArtifactHandler();
+
+        final FunctionApp app = mock(FunctionApp.class);
+        doReturn(app).when(mojoSpy).getFunctionApp();
+        doNothing().when(mojoSpy).parseConfiguration();
+        doNothing().when(mojoSpy).checkArtifactCompileVersion();
+        doReturn(slot).when(mojoSpy).updateDeploymentSlot(any(), any());
+        doCallRealMethod().when(mojoSpy).createDeploymentSlot(any(), any());
+        doReturn(null).when(mojoSpy).getResourcePortalUrl(any());
+        PowerMockito.mockStatic(FunctionUtils.class);
+        PowerMockito.when(FunctionUtils.getFunctionDeploymentSlotByName(any(), any())).thenReturn(null);
+
+        mojoSpy.doExecute();
+
+        verify(mojoSpy, times(1)).doExecute();
+        verify(mojoSpy, times(1)).createOrUpdateResource();
+        verify(mojoSpy, times(1)).createDeploymentSlot(any(), any());
+        // Will call slot update while creation as we can't modify app settings during creation
+        verify(mojoSpy, times(1)).updateDeploymentSlot(any(), any());
+        verify(artifactHandler, times(1)).publish(any());
+        verifyNoMoreInteractions(artifactHandler);
+    }
+
+    private DeployMojo getMojoFromPom() throws Exception {
+        final DeployMojo mojoFromPom = (DeployMojo) getMojoFromPom("/pom.xml", "deploy");
+        assertNotNull(mojoFromPom);
+        return mojoFromPom;
+    }
+}
---
+++ azure-maven-plugin-lib/src/main/java/com/microsoft/azure/maven/AbstractAppServiceMojo.java
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.maven;
+
+import com.microsoft.azure.AzureEnvironment;
+import com.microsoft.azure.auth.AzureAuthHelper;
+import com.microsoft.azure.auth.AzureTokenWrapper;
+import com.microsoft.azure.common.appservice.DeploymentSlotSetting;
+import com.microsoft.azure.common.appservice.DeploymentType;
+import com.microsoft.azure.common.exceptions.AzureExecutionException;
+
+import com.microsoft.azure.management.appservice.WebAppBase;
+import com.microsoft.azure.maven.auth.AzureAuthFailureException;
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Base abstract class for all Azure App Service Mojos.
+ */
+public abstract class AbstractAppServiceMojo extends AbstractAzureMojo {
+    protected static final String MAVEN_PLUGIN_POSTFIX = "-maven-plugin";
+    protected static final String PORTAL_URL_PATTERN = "%s/#@%s/resource%s";
+
+    /**
+     * Resource group of App Service. It will be created if it doesn't exist.
+     */
+    @Parameter(property = "resourceGroup", required = false)
+    protected String resourceGroup;
+
+    /**
+     * App Service name. It will be created if it doesn't exist.
+     */
+    @Parameter(property = "appName", required = false)
+    protected String appName;
+
+    /**
+     * Deployment type to deploy Web App or Function App.
+     *
+     * Supported values for Web App:
+     * <ul>
+     *      <li>FTP - {@code <resources>} specifies configurations for this kind of deployment.</li>
+     *      <li>ZIP - {@code <resources>} specifies configurations for this kind of deployment.</li>
+     *      <li>WAR - {@code <warFile>} and {@code <path>} specifies configurations for this kind of deployment.</li>
+     *      <li>JAR - {@code <jarFile>} and {@code <path>} specifies configurations for this kind of deployment.</li>
+     *      <li>AUTO - inspects {@code <packaging>} of the Maven project and uses WAR, JAR </li>
+     *      <li>NONE - does nothing</li>
+     *      <li>* defaults to AUTO if nothing is specified</li>
+     * </ul>
+     *
+     * Supported values for Function App:
+     * <ul>
+     *      <li>MSDEPLOY</li>
+     *      <li>FTP</li>
+     *      <li>ZIP</li>
+     *      <li>* defaults to ZIP if nothing is specified</li>
+     * </ul>
+     * @since 0.1.0
+     */
+    @Parameter(property = "deploymentType")
+    protected String deploymentType;
+
+    /**
+     * Resource group of App Service Plan. It will be created if it doesn't exist.
+     */
+    @Parameter(property = "appServicePlanResourceGroup")
+    protected String appServicePlanResourceGroup;
+
+    /**
+     * App Service Plan name. It will be created if it doesn't exist.
+     */
+    @Parameter(property = "appServicePlanName")
+    protected String appServicePlanName;
+
+    /**
+     * Deployment Slot. It will be created if it does not exist.
+     * It requires the web app exists already.
+     */
+    @Parameter(alias = "deploymentSlot")
+    protected DeploymentSlotSetting deploymentSlotSetting;
+
+    /**
+     * Application settings of App Service, in the form of name-value pairs.
+     * <pre>
+     * {@code
+     * <appSettings>
+     *         <property>
+     *                 <name>setting-name</name>
+     *                 <value>setting-value</value>
+     *         </property>
+     * </appSettings>
+     * }
+     * </pre>
+     */
+    @Parameter
+    protected Properties appSettings;
+
+    public String getResourceGroup() {
+        return resourceGroup;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public String getAppServicePlanResourceGroup() {
+        return appServicePlanResourceGroup;
+    }
+
+    public String getAppServicePlanName() {
+        return appServicePlanName;
+    }
+
+    public Map getAppSettings() {
+        if (appSettings == null) {
+            appSettings = new Properties();
+        }
+        return appSettings;
+    }
+
+    public DeploymentType getDeploymentType() throws AzureExecutionException {
+        return DeploymentType.fromString(deploymentType);
+    }
+
+    public DeploymentSlotSetting getDeploymentSlotSetting() {
+        return deploymentSlotSetting;
+    }
+
+    public List<Resource> getResources() {
+        return Collections.EMPTY_LIST;
+    }
+
+    public String getDeploymentStagingDirectoryPath() {
+        final String outputFolder = this.getPluginName().replaceAll(MAVEN_PLUGIN_POSTFIX, "");
+        return Paths.get(
+            this.getBuildDirectoryAbsolutePath(),
+            outputFolder, this.getAppName()
+        ).toString();
+    }
+
+    public void setDeploymentSlot(DeploymentSlotSetting slotSetting) {
+        this.deploymentSlotSetting = slotSetting;
+    }
+
+    public String getResourcePortalUrl(WebAppBase resource) throws AzureAuthFailureException {
+        final AzureTokenWrapper azureTokenWrapper = getAzureTokenWrapper();
+        final AzureEnvironment environment = azureTokenWrapper == null ?
+                AzureAuthHelper.getAzureEnvironment(auth.getEnvironment()) : azureTokenWrapper.environment();
+        final String tenantId = getAzureClient().tenantId();
+        return String.format(PORTAL_URL_PATTERN, getPortalUrl(environment), tenantId, resource.id());
+    }
+
+    private static String getPortalUrl(AzureEnvironment azureEnvironment) {
+        if (azureEnvironment == AzureEnvironment.AZURE) {
+            return "https://ms.portal.azure.com";
+        } else if (azureEnvironment == AzureEnvironment.AZURE_CHINA) {
+            return "https://portal.azure.cn";
+        } else {
+            return azureEnvironment.portal();
+        }
+    }
+}
